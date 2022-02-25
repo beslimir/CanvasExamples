96ede76ee1f9c0c9dcb457af5125d83590ab7b5a
beslimir <marin.beslo@gmail.com>
2022-02-25 12:48:08 +0100

    Path operations

--- app/src/main/java/com/example/canvaseducation/MainActivity.kt
+++ app/src/main/java/com/example/canvaseducation/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PathBasics()
+            PathOperations()
 
 //            Lines below are used for WeightPicker()
 
