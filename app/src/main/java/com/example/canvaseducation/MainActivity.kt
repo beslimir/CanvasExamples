6b86dfeb3f2f0318d6431a96d053e9ad965bc4a1
beslimir <marin.beslo@gmail.com>
2022-02-26 21:47:15 +0100

    PathEffects

--- app/src/main/java/com/example/canvaseducation/MainActivity.kt
+++ app/src/main/java/com/example/canvaseducation/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RotateARectangle()
+            PathEffects()
 
 //            Lines below are used for WeightPicker()
 
